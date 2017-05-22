3988a699ef948349ff0c64fb204ad46ca43acce3
ShingJan <yuanj.shi@hotmail.com>
2017-05-22 16:50:19 +0800

    funset not finished nor understood

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -28,24 +28,24 @@ object FunSets {
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
+    def union(s: Set, t: Set): Set = (x => contains(s,x) || contains(t,x))
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
+    def intersect(s: Set, t: Set): Set = (x => contains(s,x) && contains(t,x))
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
+    def diff(s: Set, t: Set): Set = (x => contains(s,x) && !contains(t,x))
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
+    def filter(s: Set, p: Int => Boolean): Set = (x => contains(s,x) || p(x))
   
 
   /**
@@ -70,7 +70,8 @@ object FunSets {
    * that satisfies `p`.
    */
     def exists(s: Set, p: Int => Boolean): Boolean = ???
-  
+
+
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
--- test.scala
+++ test.scala
@@ -14,4 +14,12 @@ class Upper{
 
 
 	def add(int1: Int, int2: Int): Int = int1 + int2
+
+	def sum(f: Int => Int)(a: Int, b: Int):Int = {
+		def loop(a: Int, b: Int): Int ={
+			if(a > b) 0
+			else loop(a+1, b)
+		}
+		loop(a, b)
+	}
 }
